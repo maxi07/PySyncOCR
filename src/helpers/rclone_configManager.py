365dbf8af9874cb679c897aabf8050507a44e315
Maximilian Krause <maxi.krause@me.com>
2024-01-06 18:32:27 +0100

    add pytests

--- src/helpers/rclone_configManager.py
+++ src/helpers/rclone_configManager.py
@@ -1,10 +1,41 @@
 import json
 from src.helpers.logger import logger
+from src.helpers.config import config
+
+"""
+The config should look like so:
+
+[
+    {
+        "id": "test1",
+        "local": "/home/maxi/test1",
+        "remote": "test_remote:/test1",
+        "type": "onedrive"
+    },
+    {
+        "id": "test2",
+        "local": "/home/maxi/test2",
+        "remote": "test_remote:/test2",
+        "type": "onedrive"
+    }
+]
+"""
+
+class ConfigEntry:
+    def __init__(self, id, local, remote, type):
+        self.id = id
+        self.local = local
+        self.remote = remote
+        self.type = type
+
 
 class ConfigManager:
     def __init__(self, config_file):
         self.config_file = config_file
         self.config_data = self.load_config()
+
+    def __len__(self) -> int:
+            return len(self.config_data)
 
     def load_config(self):
         try:
@@ -19,12 +50,15 @@ class ConfigManager:
         try:
             with open(self.config_file, 'w') as file:
                 json.dump(self.config_data, file, indent=2)
+                logger.debug(f"Saved config to {self.config_file}")
         except Exception as ex:
             logger.exception("Failed saving config:", ex)
 
-    def add(self, id, local, remote, type):
+    def add(self, id: str, local: str, remote: str, type):
         try:
             if not any(entry.get("id") == id for entry in self.config_data):
+                if local.startswith("/"):
+                    local = local[1:]
                 new_entry = {"id": id, "local": local, "remote": remote, "type": type}
                 self.config_data.append(new_entry)
                 self.save_config()
@@ -44,25 +78,13 @@ class ConfigManager:
         except Exception as ex:
             logger.exception(f"Failed deleting {id}:", ex)
 
-    def get(self, id):
-        return [entry for entry in self.config_data if entry.get("id") == id]
+    def get(self, id) -> ConfigEntry:
+        try:
+            entries = [ConfigEntry(**entry) for entry in self.config_data if entry.get("id") == id]
+            return entries[0] if entries else None
+        except Exception as ex:
+            logger.exception(f"Failed getting config item: {ex}")
 
-# Example usage:
-RcloneConfig = ConfigManager("src/configs/onedrive_sync_config.jso")
+RcloneConfig = ConfigManager("src/configs/onedrive_sync_config.json")
 
-# Add an entry
-RcloneConfig.add("test3", "/home/maxi/test3", "test_remote:/test3", "onedrive")
-
-# Try to add a duplicate entry (it will print a message indicating that it already exists)
-RcloneConfig.add("test3", "/home/maxi/test4", "test_remote:/test4", "onedrive")
-
-# Remove an entry by id
-RcloneConfig.delete("test2")
-
-# Access the updated config data
-print(RcloneConfig.config_data)
-
-# Get entries by id
-entries = RcloneConfig.get("test1")
-print(entries)
 logger.debug(f"Loaded {__name__} module")
---
+++ tests/config/test_rclone_config.py
@@ -0,0 +1,63 @@
+import pytest
+from shutil import copy, move
+from src.helpers.logger import logger
+from src.helpers.rclone_configManager import ConfigManager
+from src.helpers.config import config
+import os
+
+@pytest.fixture
+def use_backup():
+    copy("src/configs/onedrive_sync_config.json", "src/configs/onedrive_sync_config.json.backup")
+    logger.debug("Created backup of config.")
+    yield
+    move("src/configs/onedrive_sync_config.json.backup", "src/configs/onedrive_sync_config.json")
+    logger.debug("Reverted to original config.")
+
+@pytest.fixture
+def onedrive_conf_setup():
+    # Setup
+    config_file = str(config.get("sync_service.root_path"))
+    rclone_config = ConfigManager(config_file)
+
+    id = "test1"
+    local = "test1"
+    remote = "test1:test1/"
+    type = "onedrive"
+
+    rclone_config.add(id, local, remote, type)
+
+    # Assert
+    # assert os.path.exists(f"{config.get('sync_service.root_folder')}/{local}")
+
+    # Return the configured instance
+    return rclone_config
+
+def test_add(use_backup, onedrive_conf_setup):
+    id = "test1"
+    local = "test1"
+    remote = "test1:test1/"
+    type = "onedrive"
+
+    # assert os.path.exists("$home/PySyncOCR/test1")
+    test_item = onedrive_conf_setup.get("test1")
+    assert test_item.id == id
+    assert test_item.local == local
+    assert test_item.remote == remote
+    assert test_item.type == type
+
+
+def test_duplicate(use_backup, onedrive_conf_setup):
+    id = "test1"
+    local = "test1"
+    remote = "test1:test1/"
+    type = "onedrive"
+
+    onedrive_conf_setup.add("test1", "test1", "test1:test1/", "onedrive")
+
+    # assert os.path.exists("$home/PySyncOCR/test1")
+    assert len(onedrive_conf_setup) == 1
+
+
+def test_delete(use_backup, onedrive_conf_setup):
+    onedrive_conf_setup.delete("test1")
+    assert len(onedrive_conf_setup) == 0
