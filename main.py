9f1c28da668c7caf7b51c53ac5c78be0cf074d1d
Maximilian Krause <maxi.krause@me.com>
2024-01-07 11:54:24 +0100

    add smb server

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import threading
 from src.services.watchdog_service import FolderMonitor
 from src.services.ocr_service import OcrService
 from src.services.sync_service import SyncService
+from src.services.smb_service import MySMBServer
 from queue import Queue
 from src.helpers.config import config
 import time
@@ -21,6 +22,10 @@ if __name__ == "__main__":
     ocr_queue = Queue()
     sync_queue = Queue()
 
+    # Setup SMB
+    smb_settings = config.get("smb_service")
+    smb_server = MySMBServer(smb_settings)
+
     watchdog = FolderMonitor(root_folder, ocr_queue)
     ocr_service = OcrService(ocr_queue, sync_queue)
     sync_service = SyncService(sync_queue)
@@ -28,10 +33,12 @@ if __name__ == "__main__":
     watchdog_thread = threading.Thread(target=watchdog.start_monitoring, name="Watchdog Service")
     ocr_thread = threading.Thread(target=ocr_service.start_processing, name="OCR Service")
     sync_thread = threading.Thread(target=sync_service.start_processing, name="Sync Service")
+    smb_thread = threading.Thread(target=smb_server.start, name="SMB Service")
 
     watchdog_thread.start()
     ocr_thread.start()
     sync_thread.start()
+    smb_thread.start()
 
     try:
         while True:
@@ -45,3 +52,5 @@ if __name__ == "__main__":
 
         sync_queue.put(None)
         sync_queue.join()
+        smb_thread.join()
+
---
+++ src/services/smb_service.py
@@ -0,0 +1,39 @@
+from impacket import smbserver
+from src.helpers.logger import logger
+from impacket.ntlm import compute_lmhash, compute_nthash
+
+class MySMBServer():
+    def __init__(self, settings):
+        logger.info("Initializing SMB server...")
+        try:
+            self.server = smbserver.SimpleSMBServer()
+        except PermissionError:
+            logger.error("Cannot start SMB server. Please run the script with appropriate privileges (e.g., use sudo)")
+            return
+
+        self.settings = settings
+        logger.debug(f"Received SMB settings: {self.settings}")
+
+        try:
+            self.server.addShare(settings["share_name"].upper(), settings["share_path"], "PySyncOCR Share")
+            lmhash = compute_lmhash(settings["password"])
+            nthash = compute_nthash(settings["password"])
+            self.server.addCredential(settings["username"], 0, lmhash, nthash)
+            self.isInitialized = True
+            logger.info("Finished setting up SMB Server")
+        except Exception as e:
+            logger.error(f"Error during SMB server setup: {e}")
+            self.isInitialized = False
+    
+    def onSessionStart(self, session):
+        # You can add custom logic when a new session starts
+        logger.info(f"New session started: {session.get_client_name()}")
+
+    def start(self):
+        if self.isInitialized:
+            logger.info("SMB service has started")
+            self.server.start()
+        else:
+            logger.error("Cannot start SMB server as initialization is missing.")
+
+logger.debug(f"Loaded {__name__} module")
