3bd2ae862dc254dff3b004e228b457d85f44b53b
Maximilian Krause <maxi.krause@me.com>
2024-02-08 20:00:09 +0100

    add colored logs

--- src/helpers/logger.py
+++ src/helpers/logger.py
@@ -4,20 +4,7 @@ import coloredlogs
 
 # Set up the logger
 logger = logging.getLogger(__name__)
-"""Example usage
-```python
-logger.debug("This is a debug message")
-logger.info("This is an info message")
-logger.warning("This is a warning message")
-logger.error("This is an error message")
-logger.critical("This is a critical message")
-```"""
-
-# Set the logging level
 logger.setLevel(logging.DEBUG)
-
-# Create a formatter
-formatter = logging.Formatter("%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d] %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
 # Create a colored formatter
 colored_formatter = coloredlogs.ColoredFormatter(
@@ -39,11 +26,31 @@ console_handler.setFormatter(colored_formatter)
 # Create a file handler and set the formatter
 file_handler = logging.FileHandler("logfile.log")
 file_handler.setLevel(logging.WARNING)
-file_handler.setFormatter(formatter)
+file_handler.setFormatter(colored_formatter)
 
 # Add both handlers to the logger
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
+
+# Customize log messages before formatting
+def custom_emit(self, record):
+    try:
+        message = self.format(record)
+        if 'DEBUG' in message:
+            message = message.replace('DEBUG', '\033[35mDEBUG\033[0m')  # Pink color for DEBUG
+        if 'INFO' in message:
+            message = message.replace('INFO', '\033[32mINFO\033[0m')    # Green color for INFO
+        if 'WARNING' in message:
+            message = message.replace('WARNING', '\033[33mWARNING\033[0m')  # Yellow color for WARNING
+        if 'ERROR' in message:
+            message = message.replace('ERROR', '\033[31mERROR\033[0m')    # Red color for ERROR
+        self.stream.write(message + self.terminator)
+        self.flush()
+    except Exception:
+        self.handleError(record)
+
+
+console_handler.emit = custom_emit.__get__(console_handler, logging.StreamHandler)
 
 logger.debug(f"Loaded {__name__} module")
