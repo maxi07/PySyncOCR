f71f371e96678cd8cc95e3561e9ac26cbb919fc7
Maximilian Krause <maxi.krause@me.com>
2024-02-09 15:24:41 +0100

    add SMB share path

--- src/webserver/smb.py
+++ src/webserver/smb.py
@@ -37,11 +37,18 @@ def index():
         port = "Unknown"
 
     try:
+        share_path = config.get("smb_service.share_path")
+    except Exception as ex:
+        logger.exception(f"Error while loading share_path: {ex}")
+        share_path = "Unknown"
+
+    try:
         return render_template("smb.html",
                                status=status_text,
                                status_message=status_message,
                                username=username,
                                password=password,
-                               port=port)
+                               port=port,
+                               share_path=share_path)
     except Exception as e:
         return "Failed loading SMB settings: " + str(e)
