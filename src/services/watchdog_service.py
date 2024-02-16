dedb41aae30df6c84b7f22954e81275183c1afd4
Maximilian Krause <maxi.krause@me.com>
2024-02-16 18:05:38 +0100

    fix string for id

--- src/services/watchdog_service.py
+++ src/services/watchdog_service.py
@@ -109,7 +109,7 @@ class FileHandler(FileSystemEventHandler):
             previewfolder = 'src/webserver/static/images/pdfpreview/'
             if not os.path.exists(os.path.join(absolute_path, previewfolder)):
                 logger.debug(f"Creating folder {previewfolder}")
-            previewimage_path = previewfolder + last_inserted_id + '.jpg'
+            previewimage_path = previewfolder + str(last_inserted_id) + '.jpg'
             self.pdf_to_jpeg(item.local_file_path, os.path.join(absolute_path, previewimage_path), 128, 50)
             update_scanneddata_database(item.db_id, {'previewimage_path': previewimage_path}, self.websocket_messages_queue)
         except Exception as e:
